b864da39dc6fda0b8d9e3c1b8f4997d11be745f4
Salman Kha <code.salman.cs@gmail.com>
2022-09-05 18:41:20 +0600

    phpmyadmin apache config

--- script/phpmyadmin.sh
+++ script/phpmyadmin.sh
@@ -6,6 +6,13 @@ if type mysql >/dev/null 2>&1; then
   sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
   sudo phpenmod mbstring
   sudo systemctl restart apache2
+
+  # If phpmyadmin not configured with apache2
+  if [ ! -f /etc/apache2/conf-available/phpmyadmin.conf ]; then
+    sudo cp /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
+    sudo a2enconf phpmyadmin
+    sudo systemctl restart apache2
+  fi
 else
   echo "MySql not found"
 fi
